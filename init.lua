64dda3d9b0759185a003132f84d292a7019ee90d
Tomer Zaidenstein <ztomer@gmail.com>
2015-12-17 20:21:02 +0200

    add border

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- Hammerspoon configuration, heavily influenced by sdegutis default configuration
 
 require "pomodoor"
+--require "bar"
 
 -- init grid
 hs.grid.MARGINX 	= 0
@@ -10,7 +11,7 @@ hs.grid.GRIDHEIGHT 	= 3
 
 -- disable animation
 hs.window.animationDuration = 0
-
+--hs.hints.style = "vimperator"
 
 -- hotkey mash
 local mash       = {"ctrl", "alt"}
@@ -23,10 +24,14 @@ appCuts = {
   d = 'Dictionary',
   i = 'iterm',
   c = 'Google chrome',
-  t = 'Trello X',
+  t = 'Xccello',
   -- 4 reserved for dash shortcut 
-  q = 'Quiver',
-  e = 'emacs'
+  q = 'steam',
+  e = 'emacs',
+  r = 'reeder',
+  k = 'itunes',
+  z = 'Zim'
+ -- k = 'Chicken'
 }
 
 -- Launch applications
@@ -89,5 +94,40 @@ local function auto_tile(appName, event)
 	end
 end
 
+------------
+-- border
+------------
+border = nil
+function drawBorder()
+    if border then
+        border:delete()
+    end
+
+    local win = hs.window.focusedWindow()
+    if win == nil then return end
+
+    local f = win:frame()
+    local fx = f.x - 2
+    local fy = f.y - 2
+    local fw = f.w + 4
+    local fh = f.h + 4
+
+    border = hs.drawing.rectangle(hs.geometry.rect(fx, fy, fw, fh))
+    border:setStrokeWidth(3)
+    border:setStrokeColor({["red"]=0.75,["blue"]=0.14,["green"]=0.83,["alpha"]=0.80})
+    border:setRoundedRectRadii(5.0, 5.0)
+    border:setStroke(true):setFill(false)
+    border:setLevel("normal")
+    border:show()
+end
+
+drawBorder()
+
+windows = hs.window.filter.new(nil)
+windows:subscribe(hs.window.filter.windowFocused, function () drawBorder() end)
+windows:subscribe(hs.window.filter.windowMoved, function () drawBorder() end)
+
+
 -- start app launch watcher
 hs.application.watcher.new(auto_tile):start()
+
