3e03ee88db2ad8e8cccc9a7734779b325aff1ac2
Tomer Zaidenstein <ztomer@gmail.com>
2022-09-09 15:57:54 +0300

    .

--- tiler.lua
+++ tiler.lua
@@ -2,6 +2,8 @@
 require "homebrew"
 
 Tile = {}
+-- Good referene
+-- https://github.com/wangshub/hammerspoon-config/blob/master/window/window.lua
 -- hw.window:setFrameInScreenBounds
 -- hs.window:setSize(size)
 -- hs.window:setTopLeft(point)
