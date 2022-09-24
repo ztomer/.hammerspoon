cf49a59af58a0a17d7d59bc962dbd7b5f93e6b6c
Tomer Zaidenstein <ztomer@gmail.com>
2022-09-24 22:06:00 +0300

    Skeleton for the Tile/Area mapping

--- tiler.lua
+++ tiler.lua
@@ -12,6 +12,12 @@ require "homebrew"
 
 -- Tile Objects, allows cycling between multiple area of different sizes
 
+-- local namespace
+local Tiler = {
+    window2tile = {}, -- Maps window to tile_id and area_id
+    tile_id2tile = {} -- Maps tile_id to tile object
+}
+
 Area = {
     topleft_x = 0,
     topleft_y = 0,
@@ -20,6 +26,7 @@ Area = {
 }
 
 function Area:new(topleft_x, topleft_y, width, height)
+    -- Deines Area - a sub location in a Tile
     self.topleft_x = topleft_x
     self.topleft_y = topleft_y
     self.width = width
@@ -37,16 +44,14 @@ Tile = {
 
 }
 
--- Map between Window and area
-
 -- map between area and window
 
 function Tile:new(id, hotkey)
-    self.id = id
-    self.hotkey = hotkey
+    self._id = id
+    self._hotkey = hotkey
 end
 
-function Tile:add_area(topleft_x, topleft_y, width, height)
+function Tile:area_add(topleft_x, topleft_y, width, height)
     -- Add areas - all areas MUST be added on init
     self._areas[self._areas_num] = new
     Area(topleft_x, topleft_y, width, height)
@@ -54,16 +59,15 @@ function Tile:add_area(topleft_x, topleft_y, width, height)
     self.areas_num = self.areas_num + 1
 end
 
-function Tile:get_next(window_id)
+function Tile:area_rotate(window_id)
     -- Return the next area of the tile
     local area_idx = self._windows_area[window_id] + 1 % self._areas_num
     self.windows_area[window_id] = area_idx
-    return self._areas[area_idx]
 end
 
-function Tile:get_curr(window_id)
+function Tile:area_get_curr(window_id)
     -- Get current window position
-    local area_idx = self.windows_area[window_id]
+    local area_idx = self._windows_area[window_id]
     return self._areas[area_idx]
 end
 
@@ -73,26 +77,85 @@ function Tile:add_window(window_id)
         return
     end
     self._windows_area[window_id] = 0
+
+    -- add to global window2Tile array
+    Tiler.window2tile[window_id] = {self._id, 0}
+end
+
+function Tile:remove_window(window_id)
+    -- Remove window from the tile
+    if self._windows_area[window_id] == nil then
+        return
+    end
+    self._windows_area[window_id] = nil
+    Tiler.window2Tile[window_id] = nil
 end
 
 -- TODO: Move windows
 -- TODO: map active windows to areas
 
-function on_hot_key_activation()
-    -- Also move window
-    -- Get focused window id
+function Tile:resize_window(window_id)
+    -- Get current tile and area
+    local area = self._windows_area[window_id]
 
-    -- Get Tile, get area (can be empty)
-
-    -- Get target Tile bound to the hotkey,
-    -- If the target tile is the same as the one in the map,
-    -- cycle between areas and move the window to the next area
+    -- Resize the window
 
     -- If the target tile and the current tile are not the same,
     -- remove the window id from the current Tile, add to the target tile,
     -- set the area inde to 0 and move the window to the area coordinates
-
     -- hw.window:setFrameInScreenBounds
     -- hs.window:setSize(size)
     -- hs.window:setTopLeft(point)
+
+end
+
+function activate_move_tile(tile_id)
+    -- Activates a move_tile operation on the focused window
+
+    -- Also move window
+    -- Get focused window id
+    -- local app = hs.application.frontmostApplication()
+    local win = hs.window.focusedWindow()
+    local win_id = win:id()
+
+    -- Get Tile, get area (can be empty)
+    local window_tile_id, window_area_idx = Tiler.window2Tile[win_id]
+    local tile = Tiler.tile_id2tile(tile_id)
+
+    if tile_id == nil then
+        -- if the target tile is empty, add the window, resize, and done
+        tile.add_window(win_id)
+    elseif tile_id ~= window_tile_id then
+        -- If moving between tiles,
+        local source_tile = Tiler.tile_id2tile(window_tile_id)
+        source_tile.remove_window(window_tile_id)
+        tile.add_window(win_id)
+    else
+        -- Multiple calls to the same window on the same tile, rotate between areas
+        tile.area_roate(win_id)
+    end
+    tile.resize_window(win_id)
+
+end
+
+function activate_window_closed()
+    -- TODO: remove a window ID when an application is terminated or a window is closed
+end
+
+function activate_window_display_moved()
+    -- TODO: update the window caches when moving between hs.midi:displayName()
+end
+
+function tiler_init()
+    -- Create tiles supersets (1, 2, 3 areas)
+
+    -- Add areas to tiles
+
+    -- bind hotkeys to tile supersets
+
+    -- Bind hotkeys to tiles per superset (needs to be moved to a different function)
+
+    -- listen on window destruction for cleanups (can be very problematic if ids are recycled)
+
+    -- listen on monitor switches
 end
